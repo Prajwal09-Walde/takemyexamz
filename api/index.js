b44549ac98251af009e5da56c3578e5e4402ed58
Prajwal Walde <pbwalde98@gmail.com>
2023-10-03 16:15:08 +0530

    Third Commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import coursesRoute from './routes/courses.js';
-
+import examsRoute from './routes/exams.js';
 
 
 import cors from 'cors';
@@ -10,12 +10,15 @@ import cors from 'cors';
 const app = express();
 dotenv.config();
 
-const connect = async () => {
+async function connect () {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log('mongodb connected :)');
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Connected to mongodb :)");
     }catch (err) {
-        throw (err);
+        console.error(err);
     }
 };
 
@@ -27,6 +30,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/courses", coursesRoute);
+app.use("/api/exams", examsRoute);
 
 app.listen(8500, () => {
     connect();
